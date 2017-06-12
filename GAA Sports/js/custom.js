22dc1bf8fe16fe0d32872eb57b74076d16507835
ZanixGraphics <benjaminbroadhead@mail.com>
2017-06-12 15:40:18 +0400

    Add files via upload

---
+++ GAA Sports/js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(window).on("scroll",function(){
+  	var wn = $(window).scrollTop();
+    if(wn > 120){
+    	$(".navbar").css("background","rgba(255,0,0,1)");
+    }
+    else{
+    	$(".navbar").css("background","rgba(1,1,1,1)");
+    }
+  });
+});
